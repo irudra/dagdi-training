de0b745b586dbcd5f9cc944c7f8f7c260c46b699
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-07-15 14:06:48 +0530

    #2 : creating a dvd player
    Created a dvd player
    created only dummy message

---
+++ ClassCompiler/src/TapeDeck.java
@@ -0,0 +1,10 @@
+public class TapeDeck {
+    boolean canRecord=false;
+    void playTape(){
+        System.out.println("Tape is playing...");
+    }
+
+    void recordTape(){
+        System.out.println("Tape is Recording...");
+    }
+}
