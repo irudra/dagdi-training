cedf06bf780aaa4277dd3855f4b4bc156cae7ec4
nigam-chauhan <nigamchauhanmbd125@gmail.com>
2019-07-15 13:02:58 +0530

    #2 : creating a dvd player
    Created a dvd player
    created only dummy message

--- GameLauncher/src/src/gamelauncher/GameLauncher.java
+++ GameLauncher/src/src/gamelauncher/GameLauncher.java
@@ -3,7 +3,7 @@ package src.gamelauncher;
 public class GameLauncher {
 
     public static void main(String[] args) {
-        System.out.println("I am adding modification in branch");
+        System.out.println("I am creating a DVD player");
         GuessGame game=new GuessGame();
         game.startGame();
     }
